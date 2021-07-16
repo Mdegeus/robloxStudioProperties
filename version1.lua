461358b1d2fffd162db6ff087450d56c420d0bef
Mdegeus <70746914+Mdegeus@users.noreply.github.com>
2021-07-16 15:06:22 +0200

    Create version1.lua

---
+++ version1.lua
@@ -0,0 +1,14 @@
+{
+	BasePart = {
+		{
+			block = "Appearance",
+			properties = {
+				"Size",
+				"Position",
+				"Orientation",
+				"Origin Position",
+				"Origin Orientation",
+			}
+		}
+	}
+}
